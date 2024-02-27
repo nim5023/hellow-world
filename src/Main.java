adcc1bdcd2e8c56e5d7ac43dfa7d165ddef08c52
Nick Miller <nim5023@gmail.com>
2024-02-27 14:13:36 -0500

    reset and recommit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        System.out.println("a feature");
         System.out.println("main conflict");
+
+        System.out.println("reset and change");
+
+
+
     }
 }
